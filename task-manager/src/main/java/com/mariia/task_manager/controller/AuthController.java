2ced7f1aa7769a09864948b9d2a41506c1be7f28
Marria Rybak <marjarybak@gmail.com>
2025-02-28 13:10:47 +0100

    Implement User Authentication

---
+++ task-manager/src/main/java/com/mariia/task_manager/controller/AuthController.java
@@ -0,0 +1,48 @@
+package com.mariia.task_manager.controller;
+
+
+import com.mariia.task_manager.model.User;
+import com.mariia.task_manager.repository.UserRepository;
+import com.mariia.task_manager.security.JwtUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(UserRepository userRepository, JwtUtil jwtUtil, PasswordEncoder passwordEncoder){
+        this.userRepository=userRepository;
+        this.jwtUtil=jwtUtil;
+        this.passwordEncoder=passwordEncoder;
+
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return ResponseEntity.ok(Map.of("message", "User registered successfully"));
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user){
+        User dbUser = userRepository.findByUsername(user.getUsername()).orElseThrow();
+        if(passwordEncoder.matches(user.getPassword(), dbUser.getPassword())){
+            String token =jwtUtil.generateToken(dbUser.getUsername());
+            return ResponseEntity.ok(Map.of("token", token));
+        }
+        return ResponseEntity.status(401).body(Map.of("message", "Invalid credentials"));
+    }
+
+}
