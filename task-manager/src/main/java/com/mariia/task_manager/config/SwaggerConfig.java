78c491e8eafa89fae8de8d768651109d3102cd58
Marria Rybak <marjarybak@gmail.com>
2025-03-05 14:34:07 +0100

    Add JWT Authentication to Swagger

--- task-manager/src/main/java/com/mariia/task_manager/config/SwaggerConfig.java
+++ task-manager/src/main/java/com/mariia/task_manager/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.mariia.task_manager.config;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +19,15 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Task Manager API")
                         .version("1.0")
-                        .description("API documentation for Task Manager Application"));
+                        .description("API documentation for Task Manager Application"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("JWT Autorization header using the Bearer scheme. Enter 'Bearer' [space] and than your token. ")));
     }
 
 }
